8259368397febd8cd92e04c7877fa424e2ab0630
David <david@nyehandel.se>
2021-09-16 16:38:17 +0200

    WIP

--- src/Gateway.php
+++ src/Gateway.php
@@ -1,18 +1,70 @@
 <?php
 
 /**
- * Stripe Charge Gateway.
+ * Swish Gateway.
  */
-namespace Omnipay\Stripe;
+namespace Nyehandel\Omnipay\Swish;
+
+use Omnipay\Common\AbstractGateway;
 
 /**
- * Stripe Charge Gateway.
+ * Swish Class.
  *
- * @see \Omnipay\Stripe\AbstractGateway
- * @see \Omnipay\Stripe\Message\AbstractRequest
  *
- * @link https://stripe.com/docs/api
+ * The Swish API uses a REST-like structure. Certificates are used
+ * as the API Authorization framework. Request and response payloads are formatted as JSON.
  *
+ * ### Test Mode
+ *
+ * In order to use the test mode you need to download the test certificate and convert it to PEM files.
+ * Download the certificate from https://www.getswish.se/handel/ under the section "Testa din uppkoppling"
+ * In the zip you will find the .p12 that needs to be converted with openssl (or similar);
+ *
+ * $ openssl pkcs12 -in Swish\ Merchant\ Test\ Certificate\ 1231181189.p12 -nocerts -out certificate.key
+ * $ openssl pkcs12 -in Swish\ Merchant\ Test\ Certificate\ 1231181189.p12 -nokeys -out certificate.pem
+ *
+ * More info about converting PKCS#12 files with openssl at https://www.openssl.org/docs/manmaster/apps/pkcs12.html
+ *
+ * ### Example
+ *
+ * #### Initialize Gateway
+ *
+ * <code>
+ *   // Create a gateway for the Swish gateway
+ *   // (routes to GatewayFactory::create)
+ *   $gateway = Omnipay::create('Swish');
+ *
+ *   // Initialize the gateway
+ *   $gateway->initialize(array(
+ *               'cert' => 'certificate.pem',
+ *               'privateKey' => 'certificate.key', // Or array('certificate.key', 'password')
+ *               'caCert' => 'root_cert_from_swish.pem',
+ *               'testMode' => true
+ *   ));
+ * </code>
+ *
+ * #### Payment
+ *
+ *   $transaction = $gateway->purchase(array(
+ *       'amount'                   => '10.00',
+ *       'currency'                 => 'SEK',
+ *       'notifyUrl'                => 'https://example.com/api/swishcb/paymentrequests',
+ *       'payerAlias'               => '46701234567',
+ *       'payeeAlias'               => '1234760039',
+ *       'message'                  => 'Kingston USB Flash Drive 8 GB'
+ *   ));
+ *   $response = $transaction->send();
+ *   if ($response->isSuccessful()) {
+ *       echo "Purchase request was successfully sent!\n";
+ *       $sale_id = $response->getTransactionReference();
+ *       echo "Transaction reference = " . $sale_id . "\n";
+ *   }
+ * </code>
+ *
+ *
+ * @link https://www.getswish.se/handel/
+ * @link https://www.openssl.org/docs/manmaster/apps/pkcs12.html
+ * @see Omnipay\Swish\Message\AbstractRequest
  */
 class Gateway extends AbstractGateway
 {
@@ -21,400 +73,56 @@ class Gateway extends AbstractGateway
      */
     public function getName()
     {
-        return 'Stripe Charge';
+        return 'Swish';
     }
 
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\AuthorizeRequest
-     */
-    public function authorize(array $parameters = array())
+    public function getDefaultParameters()
     {
-        return $this->createRequest('\Omnipay\Stripe\Message\AuthorizeRequest', $parameters);
+        return array(
+            'cert'       => null,
+            'privateKey' => null,
+            'caCert'     => null,
+            'testMode'   => false,
+        );
     }
 
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\CaptureRequest
-     */
-    public function capture(array $parameters = array())
+    public function getCert()
     {
-        return $this->createRequest('\Omnipay\Stripe\Message\CaptureRequest', $parameters);
+        return $this->getParameter('cert');
     }
 
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\PurchaseRequest
-     */
+    public function setCert($value)
+    {
+        return $this->setParameter('cert', $value);
+    }
+
+    public function getPrivateKey()
+    {
+        return $this->getParameter('privateKey');
+    }
+
+    public function setPrivateKey($value)
+    {
+        return $this->setParameter('privateKey', $value);
+    }
+
+    public function getCaCert()
+    {
+        return $this->getParameter('caCert');
+    }
+
+    public function setCaCert($value)
+    {
+        return $this->setParameter('caCert', $value);
+    }
+
     public function purchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\Stripe\Message\PurchaseRequest', $parameters);
+        return $this->createRequest('\Nyehandel\Omnipay\Swish\Message\PurchaseRequest', $parameters);
     }
 
-    /**
-     * @deprecated 2.3.3:3.0.0 duplicate of \Omnipay\Stripe\Gateway::fetchTransaction()
-     * @see \Omnipay\Stripe\Gateway::fetchTransaction()
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchChargeRequest
-     */
-    public function fetchCharge(array $parameters = array())
+    public function fetchTransaction(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchChargeRequest', $parameters);
-    }
-
-    //
-    // Cards
-    // @link https://stripe.com/docs/api#cards
-    //
-
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\CreateCardRequest
-     */
-    public function createCard(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateCardRequest', $parameters);
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\UpdateCardRequest
-     */
-    public function updateCard(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\UpdateCardRequest', $parameters);
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @return \Omnipay\Stripe\Message\DeleteCardRequest
-     */
-    public function deleteCard(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\DeleteCardRequest', $parameters);
-    }
-
-    //
-    // Tokens
-    // @link https://stripe.com/docs/api#tokens
-    //
-
-    /**
-     * Creates a single use token that wraps the details of a credit card.
-     * This token can be used in place of a credit card associative array with any API method.
-     * These tokens can only be used once: by creating a new charge object, or attaching them to a customer.
-     *
-     * This kind of token is also useful when sharing clients between one platform and a connect account.
-     * Use this request to create a new token to make a direct charge on a customer of the platform.
-     *
-     * @param array $parameters parameters to be passed in to the TokenRequest.
-     * @return \Omnipay\Stripe\Message\CreateTokenRequest The create token request.
-     */
-    public function createToken(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateTokenRequest', $parameters);
-    }
-
-    /**
-     * Stripe Fetch Token Request.
-     *
-     * Often you want to be able to charge credit cards or send payments
-     * to bank accounts without having to hold sensitive card information
-     * on your own servers. Stripe.js makes this easy in the browser, but
-     * you can use the same technique in other environments with our token API.
-     *
-     * Tokens can be created with your publishable API key, which can safely
-     * be embedded in downloadable applications like iPhone and Android apps.
-     * You can then use a token anywhere in our API that a card or bank account
-     * is accepted. Note that tokens are not meant to be stored or used more
-     * than once—to store these details for use later, you should create
-     * Customer or Recipient objects.
-     *
-     * @param array $parameters
-     *
-     * @return \Omnipay\Stripe\Message\FetchTokenRequest
-     */
-    public function fetchToken(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchTokenRequest', $parameters);
-    }
-
-    /**
-     * Create Plan
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CreatePlanRequest
-     */
-    public function createPlan(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreatePlanRequest', $parameters);
-    }
-
-    /**
-     * Fetch Plan
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchPlanRequest
-     */
-    public function fetchPlan(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchPlanRequest', $parameters);
-    }
-
-    /**
-     * Delete Plan
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\DeletePlanRequest
-     */
-    public function deletePlan(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\DeletePlanRequest', $parameters);
-    }
-
-    /**
-     * List Plans
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\ListPlansRequest
-     */
-    public function listPlans(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\ListPlansRequest', $parameters);
-    }
-
-    /**
-     * Create Subscription
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CreateSubscriptionRequest
-     */
-    public function createSubscription(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateSubscriptionRequest', $parameters);
-    }
-
-    /**
-     * Fetch Subscription
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchSubscriptionRequest
-     */
-    public function fetchSubscription(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchSubscriptionRequest', $parameters);
-    }
-
-    /**
-     * Update Subscription
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\UpdateSubscriptionRequest
-     */
-    public function updateSubscription(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\UpdateSubscriptionRequest', $parameters);
-    }
-
-    /**
-     * Cancel Subscription
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CancelSubscriptionRequest
-     */
-    public function cancelSubscription(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CancelSubscriptionRequest', $parameters);
-    }
-
-    /**
-     * Fetch Schedule Subscription
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchSubscriptionSchedulesRequest
-     */
-    public function fetchSubscriptionSchedules(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchSubscriptionSchedulesRequest', $parameters);
-    }
-
-    /**
-     * Fetch Event
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchEventRequest
-     */
-    public function fetchEvent(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchEventRequest', $parameters);
-    }
-
-    /**
-     * Fetch Invoice Lines
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchInvoiceLinesRequest
-     */
-    public function fetchInvoiceLines(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchInvoiceLinesRequest', $parameters);
-    }
-
-    /**
-     * Fetch Invoice
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchInvoiceRequest
-     */
-    public function fetchInvoice(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchInvoiceRequest', $parameters);
-    }
-
-    /**
-     * List Invoices
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\ListInvoicesRequest
-     */
-    public function listInvoices(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\ListInvoicesRequest', $parameters);
-    }
-
-    /**
-     * Create Invoice Item
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CreateInvoiceItemRequest
-     */
-    public function createInvoiceItem(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateInvoiceItemRequest', $parameters);
-    }
-
-    /**
-     * Fetch Invoice Item
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchInvoiceItemRequest
-     */
-    public function fetchInvoiceItem(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchInvoiceItemRequest', $parameters);
-    }
-
-    /**
-     * Delete Invoice Item
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\DeleteInvoiceItemRequest
-     */
-    public function deleteInvoiceItem(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\DeleteInvoiceItemRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CreateSourceRequest
-     */
-    public function createSource(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateSourceRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\AttachSourceRequest
-     */
-    public function attachSource(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\AttachSourceRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchSourceRequest
-     */
-    public function detachSource(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\DetachSourceRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\FetchSourceRequest
-     */
-    public function fetchSource(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchSourceRequest', $parameters);
-    }
-
-    /**
-     * Create a completePurchase request.
-     *
-     * @param array $parameters
-     * @return \Omnipay\Stripe\Message\CompletePurchaseRequest
-     */
-    public function completePurchase(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CompletePurchaseRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function createCoupon(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\CreateCouponRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function fetchCoupon(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\FetchCouponRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function deleteCoupon(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\DeleteCouponRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function updateCoupon(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\UpdateCouponRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function listCoupons(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Stripe\Message\ListCouponsRequest', $parameters);
+        return $this->createRequest('\Nyehandel\Omnipay\Swish\Message\FetchTransactionRequest', $parameters);
     }
 }
---
+++ src/Message/PurchaseResponse.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Nyehandel\Omnipay\Swish\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
+
+class PurchaseResponse extends AbstractResponse
+{
+    protected $statusCode;
+    protected $response;
+
+    public function __construct(RequestInterface $request, $response, $data, $statusCode)
+    {
+        parent::__construct($request, $data);
+        $this->statusCode = $statusCode;
+        $this->response = $response;
+    }
+
+    public function isSuccessful()
+    {
+        return $this->getCode() == 200 || $this->getCode() == 201;
+    }
+
+    public function getTransactionReference()
+    {
+        $location = $this->response->getHeader('location');
+        if (!empty($location)) {
+            $urlParts = explode('/', $location);
+
+            return end($urlParts);
+        }
+    }
+
+    public function getMessage()
+    {
+        if (isset($this->data[0]['errorMessage'])) {
+            return $this->data[0]['errorMessage'];
+        }
+    }
+
+    public function getCode()
+    {
+        return $this->statusCode;
+    }
+}
